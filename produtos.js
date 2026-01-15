785b873f3dfdc3aa82257b643d32446792762bec
yasmin <yasmintamires27@gmail.com>
2026-01-15 20:58:49 -0300

    feat: adicionei preços e novos produtos

--- produtos.js
+++ produtos.js
@@ -8,205 +8,320 @@ let semiJoias = [
     {
         id: 2,
         nome: "Brincos banhado à ouro",
-        preco: "R$95.00",
+        preco: "R$120.00",
         img: "./semijoias/brinco2.jpg"
     },
     {
         id: 3,
         nome: "Brincos de prata",
-        preco: "R$95.00",
+        preco: "R$70.00",
         img: "./semijoias/brinco3.jpg"
     },
     {
         id: 4,
         nome: "Brincos banhado à ouro",
+        preco: "R$95.00",
         img: "./semijoias/brinco4.jpg"
     },
     {
         id: 5,
         nome: "Brincos de prata",
+        preco: "R$80.00",
         img: "./semijoias/brinco5.jpg"
     },
     {
         id: 6,
         nome: "Brincos banhado à ouro",
+        preco: "R$95.00",
         img: "./semijoias/brinco6.jpg"
     },
     {
         id: 7,
         nome: "Brincos banhado à ouro",
+        preco: "R$60.00",
         img: "./semijoias/brinco7.jpg"
     },
     {
         id: 8,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco8.jpg"
     },
     {
         id: 9,
         nome: "Brincos de prata",
+        preco: "R$120.00",
         img: "./semijoias/brinco9.jpg"
     },
     {
         id: 10,
         nome: "Brincos banhado à ouro",
+        preco: "R$95.00",
         img: "./semijoias/brinco10.jpg"
     },
     {
         id: 11,
         nome: "Brincos banhado à ouro",
+        preco: "R$85.00",
         img: "./semijoias/brinco11.jpg"
     },
     {
         id: 12,
         nome: "Brincos banhado à ouro",
+        preco: "R$115.00",
         img: "./semijoias/brinco12.jpg"
     },
     {
         id: 13,
         nome: "Brincos banhado à ouro",
-        img: "./semijoias/brinco13.jpg"
+        preco: "R$85.00",
+        img: "./semijoias/brinco13.png"
     },
     {
         id: 14,
         nome: "Brincos banhado à ouro",
+        preco: "R$130.00",
         img: "./semijoias/brinco14.png"
     },
     {
         id: 15,
         nome: "Brincos banhado à ouro",
+        preco: "R$95.00",
         img: "./semijoias/brinco15.png"
     },
     {
         id: 16,
         nome: "Brincos banhado à ouro",
+        preco: "R$95.00",
         img: "./semijoias/brinco16.png"
     },
     {
         id: 17,
         nome: "Brincos banhado à ouro",
+        preco: "R$140.00",
         img: "./semijoias/brinco17.png"
     },
     {
         id: 18,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco18.png"
     },
     {
         id: 19,
         nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
         img: "./semijoias/brinco19.png"
     },
     {
         id: 20,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco20.png"
     },
     {
         id: 21,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco21.png"
     },
     {
         id: 22,
         nome: "Brincos banhado à ouro",
+        preco: "R$130.00",
         img: "./semijoias/brinco22.png"
     },
     {
         id: 23,
         nome: "Brincos banhado à ouro",
+        preco: "R$130.00",
         img: "./semijoias/brinco23.png"
     },
     {
         id: 24,
-        nome: "Brincos banhado à ouro",
-        img: "./semijoias/brinco24.png"
+        nome: "Brincos de prata",
+        preco: "R$90.00",
+        img: "./semijoias/brinco24.jpg"
     },
     {
         id: 25,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco25.png"
     },
     {
         id: 26,
         nome: "Brincos banhado à ouro",
+        preco: "R$85.00",
         img: "./semijoias/brinco26.png"
     },
     {
         id: 27,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco27.png"
     },
     {
         id: 28,
         nome: "Brincos banhado à ouro",
+        preco: "R$115.00",
         img: "./semijoias/brinco28.png"
     },
     {
         id: 29,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco29.png"
     },
     {
         id: 30,
         nome: "Brincos banhado à ouro",
+        preco: "R$115.00",
         img: "./semijoias/brinco30.png"
     },
     {
         id: 31,
         nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
         img: "./semijoias/brinco31.png"
     },
     {
         id: 32,
         nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
         img: "./semijoias/brinco32.png"
     },
     {
         id: 33,
         nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
         img: "./semijoias/brinco33.png"
     },
     {
         id: 34,
         nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
         img: "./semijoias/brinco34.png"
     },
     {
         id: 35,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco35.png"
     },
     {
         id: 36,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco36.png"
     },
     {
         id: 37,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco37.png"
     },
     {
         id: 38,
         nome: "Brincos banhado à ouro",
+        preco: "R$120.00",
         img: "./semijoias/brinco38.png"
     },
     {
         id: 39,
-        nome: "Colar banhado à ouro",
-        preco: 95.00,
-        img: "./semijoias/colar1.png"
+        nome: "Brincos banhado à ouro",
+        preco: "120.00",
+        img: "./semijoias/brinco39.jpg"
     },
     {
         id: 40,
-        nome: "Colar banhado à ouro",
-        img: "./semijoias/colar2.png"
+        nome: "Brincos banhado à ouro",
+        preco: "105.00",
+        img: "./semijoias/brinco40.jpg"
     },
     {
         id: 41,
+        nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
+        img: "./semijoias/brinco41.jpg"
+    },
+    {
+        id: 42,
+        nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
+        img: "./semijoias/brinco42.jpg"
+    },
+    {
+        id: 43,
+        nome: "Brincos banhado à ouro",
+        preco: "R$105.00",
+        img: "./semijoias/brinco43.jpg"
+    },
+    {
+        id: 44,
+        nome: "Brincos banhado à ouro",
+        preco: "R$130.00",
+        img: "./semijoias/brinco44.png"
+    },
+    {
+        id: 45,
+        nome: "Brincos banhado à ouro",
+        preco: "R$115.00",
+        img: "./semijoias/brinco45.jpg"
+    },
+    {
+        id: 46,
+        nome: "Brincos banhado à ouro",
+        preco: "R$140.00",
+        img: "./semijoias/brinco46.jpg"
+    },
+    {
+        id: 47,
+        nome: "Brincos banhado à ouro",
+        preco: "R$130.00",
+        img: "./semijoias/brinco47.jpg"
+    },
+    {
+        id: 48,
+        nome: "Tornozeleira banhado à ouro",
+        preco: "R$130.00",
+        img: "./semijoias/tornozeleira.png"
+    },
+    {
+       id: 49,
+        nome: "Pulseira banhado à ouro",
+        preco: "R$120.00",
+        img: "./semijoias/pulseira1.png" 
+    },
+    {
+        id: 50,
+        nome: "Pulseira banhado à ouro",
+        preco: "R$130.00",
+        img: "./semijoias/pulseira2.png" 
+    },
+    {
+        id: 51,
+        nome: "Pulseira de prata",
+        preco: "R$150.00",
+        img: "./semijoias/pulseira3.png" 
+    },
+    {
+        id: 52,
+        nome: "Pulseira de prata",
+        preco: "R$130.00",
+        img: "./semijoias/pulseira4.png" 
+    },
+    {
+        id: 53,
         nome: "Colar banhado à ouro",
-        img: "./semijoias/pulseira1.png"
+        preco: "R$140.00",
+        img: "./semijoias/colar1.png" 
+    },
+    {
+        id: 54,
+        nome: "Colar banhado à ouro",
+        preco: "R$160.00",
+        img: "./semijoias/colar2.png" 
     }
 
 ];
